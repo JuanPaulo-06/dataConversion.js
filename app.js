cfd35837671e7b71bc7558aca743ea4aa98c5bf6
Juan Paulo Passi <passipaulo14@gmail.com>
2024-09-15 16:26:32 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+// Convert a String to a Number
+let price = "200";
+
+let priceAsInt = parseInt(price);
+let priceAsNumber = Number(price);
+let priceAsPlus = +price;
+
+console.log(priceAsInt);  
+console.log(priceAsNumber);  
+console.log(priceAsPlus);  
+
+// Convert a Number to a String 
+let distance = 150;
+
+let distanceStr1 = distance.toString();
+
+let distanceStr2 = String(distance);
+
+console.log(distanceStr1);
+console.log(distanceStr2);
+
+// Convert a String to Decimal Number
+let weight = "72.5";    
+let weightInDecimal = parseFloat(weight);    
+
+console.log(weightInDecimal);
